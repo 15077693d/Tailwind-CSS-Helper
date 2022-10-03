25c216e3c120e9560b1591d8f9761e049ce7e3b9
15077693d <oscaryiu.lapsang@gmail.com>
2022-10-03 18:40:26 +0800

    can download test.json

--- code.ts
+++ code.ts
@@ -11,23 +11,19 @@ figma.showUI(__html__);
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage = msg => {
+figma.ui.onmessage = (msg) => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+  if (msg.type === "export") {
+    // const paintStylesInTheme = figma.getLocalPaintStyles();
+    // paintStylesInTheme.map((paintStyle) => {
+    //   const { r, g, b } = (paintStyle.paints[0] as SolidPaint).color;
+    //   console.log(paintStyle.name, rgbToHex(r, g, b));
+    // });
+    figma.ui.postMessage({ stylesInJson: { a: 123 }, type: "stylesInJson" });
   }
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+  //figma.closePlugin();
 };
